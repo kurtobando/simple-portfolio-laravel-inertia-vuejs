bce74c9e8fb843f246495b1441d48ce75dcde64d
Kurt Obando <kurtobando.developer@gmail.com>
2022-10-22 19:19:00 +0800

    install lucide icons

--- resources/js/Components/SocialMediaLinks.vue
+++ resources/js/Components/SocialMediaLinks.vue
@@ -1,10 +1,29 @@
 <template>
-    <ul>
-        <li>github</li>
-        <li>linkedin</li>
+    <ul class="flex gap-4">
+        <li>
+            <a
+                href="https://github.com/kurtobando/"
+                target="_blank">
+                <Github />
+            </a>
+        </li>
+        <li>
+            <a
+                href="https://www.linkedin.com/in/kurt-obando/"
+                target="_blank">
+                <Linkedin />
+            </a>
+        </li>
     </ul>
 </template>
 
 <script>
-export default {};
+import { Linkedin, Github } from 'lucide-vue-next';
+
+export default {
+    components: {
+        Linkedin,
+        Github,
+    },
+};
 </script>
