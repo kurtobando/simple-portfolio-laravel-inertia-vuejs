9e890901a3a172f6acb74c57a9feeb46425b4f4e
Kurt Obando <kurtobando.developer@gmail.com>
2022-10-22 17:10:25 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from "path";
+
+export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 5000,
+    },
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, "./resources/js"),
+        },
+    },
+});
