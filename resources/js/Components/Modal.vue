d83f61d256ccce7969de599b345c931a3bd10d95
Kurt Obando <kurtobando.developer@gmail.com>
2022-10-27 23:55:46 +0800

    add overflow css, for modal component

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -3,13 +3,18 @@
         <div
             class="relative z-10"
             v-if="isOpen">
-            <div class="fixed inset-0 bg-black bg-opacity-60 transition-opacity"></div>
-            <div class="fixed inset-0 z-10">
+            <div class="fixed inset-0 bg-black bg-opacity-60"></div>
+            <div class="fixed inset-0 z-10 overflow-y-scroll">
                 <div class="flex min-h-full items-center justify-center p-4">
                     <div
                         ref="modalRef"
                         class="relative rounded bg-white shadow p-6 md:p-10 w-full md:w-2/3 xl:w-2/3 2xl:w-1/2">
                         <slot />
+                        <a
+                            @click="onToggle"
+                            class="block underline text-sm text-center text-slate-600 my-8 mb-4 md:hidden">
+                            Close Window
+                        </a>
                     </div>
                 </div>
             </div>
