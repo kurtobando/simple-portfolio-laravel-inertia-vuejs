f5bca860965b7f802761690c3725476075b2c5db
Kurt Obando <kurtobando.developer@gmail.com>
2023-08-19 12:57:31 +0800

    ignore ziggy routes in resource directory

--- resources/js/ziggy.js
+++
@@ -1,7 +0,0 @@
-const Ziggy = {"url":"http:\/\/simple-portfolio-laravel-inertia-vuejs.test","port":null,"defaults":{},"routes":{"sanctum.csrf-cookie":{"uri":"sanctum\/csrf-cookie","methods":["GET","HEAD"]},"telescope":{"uri":"telescope\/{view?}","methods":["GET","HEAD"],"wheres":{"view":"(.*)"}},"ignition.healthCheck":{"uri":"_ignition\/health-check","methods":["GET","HEAD"]},"ignition.executeSolution":{"uri":"_ignition\/execute-solution","methods":["POST"]},"ignition.updateConfig":{"uri":"_ignition\/update-config","methods":["POST"]},"about":{"uri":"\/","methods":["GET","HEAD"]},"projects":{"uri":"projects","methods":["GET","HEAD"]},"projects.show":{"uri":"projects\/{slug}","methods":["GET","HEAD"]},"contact":{"uri":"contact","methods":["GET","HEAD"]},"contact.store":{"uri":"contact","methods":["POST"]},"sign-in":{"uri":"sign-in","methods":["GET","HEAD"]},"sign-in.store":{"uri":"sign-in","methods":["POST"]},"sign-out":{"uri":"sign-out","methods":["POST"]},"dashboard":{"uri":"dashboard","methods":["GET","HEAD"]}}};
-
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-    Object.assign(Ziggy.routes, window.Ziggy.routes);
-}
-
-export { Ziggy };
